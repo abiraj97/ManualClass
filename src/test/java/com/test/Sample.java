40fbaf410f76cbb2db5f9bf9aecf5400bb67f09d
abiraj97 <abiakashraj250@gmail.com>
2023-04-13 15:39:15 +0530

    GIT Class

--- src/test/java/com/test/Sample.java
+++ src/test/java/com/test/Sample.java
@@ -14,4 +14,16 @@ public class Sample {
 	private void Sample3() {
          System.out.println("S3");
 	}
+	
+	private void Manual1() {
+		 System.out.println("M1");
+	}
+	
+	private void Manual2() {
+		 System.out.println("M2");
+	}
+	
+	private void Manual3() {
+		 System.out.println("M3");
+	}
 }
